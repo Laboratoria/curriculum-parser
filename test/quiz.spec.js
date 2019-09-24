5424ae579297ef4bcf0af3a8b499f52b141aad00
Lupo Montero <lupomontero@gmail.com>
2019-09-23 19:40:56 -0500

    refactor(quiz): Simplifies code and adds stricter errors

--- test/quiz.spec.js
+++ test/quiz.spec.js
@@ -4,16 +4,56 @@ const common = require('../lib/common');
 
 
 describe('quiz', () => {
+  it('should throw when question is missing options', () => {
+    const quizReadme = helpers.readFixtureFile('00-quiz-missing-options/README.md');
+    const parsedQuizReadme = common.parseReadme(quizReadme, {
+      tipo: 'type',
+      formato: 'format',
+      duración: 'duration',
+    });
+    expect(() => quiz(parsedQuizReadme.body))
+      .toThrowError(/Missing answers/i);
+  });
+
+  it('should throw when question is missing answers', () => {
+    const quizReadme = helpers.readFixtureFile('00-quiz-missing-answers/README.md');
+    const parsedQuizReadme = common.parseReadme(quizReadme, {
+      tipo: 'type',
+      formato: 'format',
+      duración: 'duration',
+    });
+    expect(() => quiz(parsedQuizReadme.body))
+      .toThrowError(/Missing answers/i);
+  });
+
+  it('should throw when question is missing solution', () => {
+    const quizReadme = helpers.readFixtureFile('00-quiz-missing-solution/README.md');
+    const parsedQuizReadme = common.parseReadme(quizReadme, {
+      tipo: 'type',
+      formato: 'format',
+      duración: 'duration',
+    });
+    expect(() => quiz(parsedQuizReadme.body))
+      .toThrowError(/Missing solution/i);
+  });
+
+  it('should throw when unexpected element in question', () => {
+    const quizReadme = helpers.readFixtureFile('00-quiz-unexpected-element/README.md');
+    const parsedQuizReadme = common.parseReadme(quizReadme, {
+      tipo: 'type',
+      formato: 'format',
+      duración: 'duration',
+    });
+    expect(() => quiz(parsedQuizReadme.body))
+      .toThrowError(/Unexpected element/i);
+  });
+
   it('should parse quiz from html created from README.md', () => {
     const quizReadme = helpers.readFixtureFile('00-quiz/README.md');
     const parsedQuizReadme = common.parseReadme(quizReadme, {
       tipo: 'type',
-      type: 'type',
       formato: 'format',
-      format: 'format',
       duración: 'duration',
-      duration: 'duration',
-      duração: 'duration',
     });
     const quizQuestions = quiz(parsedQuizReadme.body);
     expect(Array.isArray(quizQuestions)).toBe(true);
