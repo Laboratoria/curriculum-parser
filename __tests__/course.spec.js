f5cfcba27e876ba6cb2d9ed90bad375dd92f5d7c
Alfredo González <alfredo.arosteguy@gmail.com>
2022-03-17 16:48:16 -0300

    feat(course): Make use of getImages function on course.js module, includes tests

--- __tests__/course.spec.js
+++ __tests__/course.spec.js
@@ -1,10 +1,20 @@
+const fs = require('fs');
+const path = require('path');
 const mongoose = require('mongoose');
 const models = require('@laboratoria/models')(mongoose);
+const nock = require('nock');
+const sharp = require('sharp');
 const helpers = require('./helpers');
 const course = require('../lib/course');
 
 
 describe('course', () => {
+  beforeEach(() => {
+    sharp.mockClear();
+    sharp().resize.mockClear();
+    sharp().resize().toBuffer.mockClear();
+  });
+
   it('should reject with error when dir doesnt exist', () => (
     course('foo', models)
       .catch((err) => {
@@ -121,4 +131,61 @@ describe('course', () => {
     })
       .then(result => expect(result.syllabus).toMatchSnapshot())
   ));
+
+  it('should have null cover and thumb if course has no images on README or no thumb.png file', async () => {
+    const data = await course(helpers.resolveFixturePath('02-course-no-tags'), models, {
+      repo: 'Laboratoria/bootcamp',
+      version: '2.0.0',
+      track: 'js',
+      locale: 'es-ES',
+    });
+    expect(data.cover).toBeNull();
+    expect(data.thumb).toBeNull();
+  });
+
+  it('should create a thumbnail when file not present and has cover', async () => {
+    const p = helpers.resolveFixturePath('04-course-with-image-in-readme');
+    const thumbPath = path.join(p, 'thumb.png');
+
+    if (fs.existsSync(thumbPath)) {
+      fs.unlinkSync(thumbPath);
+    }
+
+    const scope = nock('https://www.101computing.net')
+      .get('/wp/wp-content/uploads/Luhn-Algorithm.png')
+      .reply(200, 'xxxx');
+
+    const data = await course(p, models, {
+      repo: 'Laboratoria/bootcamp',
+      version: '2.0.0',
+      track: 'js',
+      locale: 'es-ES',
+    });
+
+    expect(fs.existsSync(thumbPath)).toBe(true);
+    expect(typeof data.thumb).toBe('string');
+    expect(data.thumb).toMatch(/^data:image\/png;base64,/);
+    fs.unlinkSync(thumbPath);
+    scope.done();
+    expect(sharp).toHaveBeenCalled();
+    expect(sharp().resize).toHaveBeenCalledWith(395);
+    expect(sharp().resize().toBuffer).toHaveBeenCalled();
+    expect(data.cover)
+      .toBe('https://www.101computing.net/wp/wp-content/uploads/Luhn-Algorithm.png');
+    expect(data.thumb).toBe('data:image/png;base64,data:image/png;base64,xxxx');
+  });
+
+  it('should use thumb.png file if present on dir', async () => {
+    const data = await course(helpers.resolveFixturePath('04-course-with-thumb.png-file'), models, {
+      repo: 'Laboratoria/bootcamp',
+      version: '2.0.0',
+      track: 'js',
+      locale: 'es-ES',
+    });
+
+    expect(data.thumb).not.toBeNull();
+    expect(typeof data.thumb).toBe('string');
+    expect(data.thumb).toMatch(/^data:image\/png;base64,/);
+    expect(data.cover).toBeNull();
+  });
 });
---
+++ __tests__/fixtures/04-course-with-image-in-readme/README.md
@@ -0,0 +1,12 @@
+# El título del curso
+
+![image](https://www.101computing.net/wp/wp-content/uploads/Luhn-Algorithm.png)
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+culpa qui officia deserunt mollit anim id est laborum.
+
+Tags: `foo`, `bar`, `baz`
